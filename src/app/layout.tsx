ff9180075f02bc12aa7fda775b5f6a466b707264
leojuriolli7 <leojuriolli@gmail.com>
2025-07-06 09:59:00 -0300

    feat: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,34 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "ZOE STORE",
-  description: "Peças que vestem elegância",
+  description: "Peças que vestem elegância, Enviamos para todo o Brasil.",
+  openGraph: {
+    title: "ZOE STORE",
+    description: "Peças que vestem elegância, Enviamos para todo o Brasil.",
+    url: "https://zoestore-alpha.vercel.app/",
+    siteName: "ZOE STORE",
+    images: [
+      {
+        url: "/zoe_store_logo.jpg",
+        width: 800,
+        height: 600,
+        alt: "ZOE STORE Logo",
+      },
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "ZOE STORE",
+    description: "Peças que vestem elegância, Enviamos para todo o Brasil.",
+    images: ["/zoe_store_logo.jpg"],
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
