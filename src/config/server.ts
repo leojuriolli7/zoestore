f3ec994f0a64a88d1acc18eee468fe0084f2fd92
leojuriolli7 <leojuriolli@gmail.com>
2025-07-07 12:52:19 -0300

    feat: cron job for deleting images

--- src/config/server.ts
+++ src/config/server.ts
@@ -7,5 +7,6 @@ export const appServerConfig = {
   auth: {
     adminKeyCookieName: "zoe_store__admin_key",
     adminKey: process.env.ADMIN_KEY!,
+    cronSecret: process.env.CRON_SECRET!,
   },
 };
