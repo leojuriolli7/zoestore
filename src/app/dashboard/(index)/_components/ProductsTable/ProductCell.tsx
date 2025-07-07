b57b3d69fe85fba2aca6fca6d6bda20217711212
leojuriolli7 <leojuriolli@gmail.com>
2025-07-07 15:15:11 -0300

    fix: dashboard image rendering

--- src/app/dashboard/(index)/_components/ProductsTable/ProductCell.tsx
+++ src/app/dashboard/(index)/_components/ProductsTable/ProductCell.tsx
@@ -1,18 +1,22 @@
-/* eslint-disable @next/next/no-img-element */
 import { Products } from "@/query/products/types";
+import Image from "next/image";
 
 interface ProductCellProps {
   product: Products.Product;
 }
 
 export function ProductCell({ product }: ProductCellProps) {
+  console.log(" product:", product);
   return (
     <div className="flex items-center gap-4 p-2">
-      <img
-        src={product.image_url}
-        alt={product.name}
-        className="w-12 aspect-[2/3] object-cover rounded shrink-0"
-      />
+      <div className="w-12 h-[72px] relative">
+        <Image
+          fill
+          src={product.image_url}
+          alt={product.name}
+          className="object-cover rounded shrink-0"
+        />
+      </div>
       <div className="flex-1">
         <h2 className="font-bold text-md">{product.name}</h2>
         <p className="text-card-foreground/80 text-sm mb-1 overflow-hidden overflow-ellipsis line-clamp-2">
