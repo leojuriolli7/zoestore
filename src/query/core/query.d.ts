7a2cc3a1ffc7d1134d3b4a747cc65f8dbbc7cbdc
leojuriolli7 <leojuriolli@gmail.com>
2025-07-13 15:31:16 -0300

    fix: query typing

--- src/query/core/query.d.ts
+++ src/query/core/query.d.ts
@@ -1,6 +1,24 @@
-export declare namespace API {
+import "@tanstack/react-query";
+import type { NextResponse } from "next/server";
+import type { BaseError } from "../errors/BaseError";
+
+declare module "@tanstack/react-query" {
+  interface Register {
+    defaultError: BaseError;
+  }
+}
+
+export namespace API {
   type InfiniteListResult<T> = {
     results: T[];
     nextCursor: number | null;
   };
+
+  type RequestSuccess<T> = NextResponse<T>;
+
+  type RequestError = NextResponse<{
+    error: BaseError;
+  }>;
+
+  type Response<T> = RequestSuccess<T> | RequestError;
 }
